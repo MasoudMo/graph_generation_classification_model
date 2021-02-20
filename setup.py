8135586bd447108ebd3231367b409a28a32305b0
mmokhtari <mmokhtari@triumf.ca>
2021-02-19 19:18:30 -0800

    Initial commit with project directory structure (cookiecutter)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Integrating the graph generation process into the classification process using GNNs.',
+    author='Masoud Mokhtari',
+    license='MIT',
+)
