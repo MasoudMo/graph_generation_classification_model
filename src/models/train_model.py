c85ca8e30ab501a04acd35acbeb7a3db3a14aa2c
mmokhtari <mmokhtari@triumf.ca>
2021-02-25 22:37:52 -0800

    Added the first training model code

---
+++ src/models/train_model.py
@@ -0,0 +1,17 @@
+from dataset import PtbEcgDataset
+import click
+
+@click.command()
+@click.argument('train_data_dir', type=click.Path(exists=True))
+@click.argument('train_label_dir', type=click.Path(exists=True))
+def train(train_data_dir, train_label_dir):
+
+    dataset = PtbEcgDataset(input_data_csv_file=train_data_dir, input_label_csv_file=train_label_dir)
+
+    print('Training dataset has {} samples'.format(len(dataset)/15))
+
+    for data, label in dataset:
+        print('hello')
+
+if __name__ == "__main__":
+    train()
